7396a1882097493bbf496bdf85b445f9a23920e4
radness <yhs5128@gmail.com>
2020-02-05 23:24:21 +0900

    directory cleanup

---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import LandingPage from './LandingPage';
+import About from './About';
+import Contact from './Contact';
+import Project from './Project';
+import Resume from './Resume';
+
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/aboutme" component={About} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Project} />
+    <Route path="/resume" component={Resume} />
+  </Switch>
+)
+
+export default Main;
