b52c96e2772e5e705c448c1db1df8984ef83d277
radness <yhs5128@gmail.com>
2020-02-15 13:45:03 +0900

    JSX 내에서는 class대신 className을 사용해야 한다.

--- src/components/test.js
+++ src/components/test.js
@@ -1,17 +1,35 @@
 import React, { Component, Fragment } from 'react';
+import '../App.css';
 
 class test extends Component {
 
     render() {
         const name='radness';
 
+        const style = {
+            // DOM 요소에 스타일을 적용할 떄는 문자열 형태로 넣는 것이 아니라
+            // 객체 형태로 넣어줘야 합니다.
+            backgroundColor: 'black',
+            color: 'aqua',
+            fontSIze: '48px',
+            fontWeight: 'bold',
+            padding: 16 // 단위를 생략하면 px로 저장됩니다.
+        };
+
         return (
             <Fragment>
                 <div>
                     {name === 'radness' ? (
                         <h1>삼항연산자 테스트 : {name}</h1>
-                    ) : (<h2>삼항연산자 테스트</h2>)
+                    ) : null
                     }
+                    {name === 'radness' && <h1>{name} 입니다.</h1>}
+                </div>
+                <div style={style}>
+                    {name}
+                </div>
+                <div className="react">
+                    {name}
                 </div>
             </Fragment>
         );
